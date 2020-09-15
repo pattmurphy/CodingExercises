99b7898328a1de3a1bc8170ae31a6de072ecf3e5
Patrick Murphy <43220681+DiningRoomChair@users.noreply.github.com>
2020-09-15 00:13:59 -0400

    fixes

---
+++ Leetcode/Easy/Python/925 - Long Pressed Name.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isLongPressedName(self, name: str, typed: str) -> bool:
+        for x in range(len(name)):
+            for y in range(len(typed)):
+                # check if name[x] == typed[y] and if that letter is repeated
+                # if repeated less in x than y continue while keeping track of the indicies
+                print("need to finish")
+
+        return("true or false")
