4d9f7c3dc5ab08de45473d94ada0c2c801d3b9bb
Patrick Murphy <43220681+DiningRoomChair@users.noreply.github.com>
2020-09-22 14:39:26 -0400

    925 - Long Pressed Name

--- Leetcode/Easy/Python/925 - Long Pressed Name.py
+++ Leetcode/Easy/Python/925 - Long Pressed Name.py
@@ -1,9 +1,30 @@
 class Solution:
     def isLongPressedName(self, name: str, typed: str) -> bool:
+        nameCount, y = 1, 0
         for x in range(len(name)):
-            for y in range(len(typed)):
-                # check if name[x] == typed[y] and if that letter is repeated
-                # if repeated less in x than y continue while keeping track of the indicies
-                print("need to finish")
+            if x < len(name)-1:
+                if name[x] == name[x+1]:
+                    nameCount += 1
+                    continue
+            if y > len(typed) -1 or name[x] != typed[y]:
+                return False
+            typedCount = 0
+            while name[x] == typed[y]:
+                typedCount += 1
+                y += 1
+                if y == len(typed):
+                    break
+            if nameCount > typedCount:
+                return False
+            nameCount = 1
+        if y < len(typed):
+            return False
+        return True
 
-        return("true or false")
+solution = Solution()
+print(solution.isLongPressedName("pyplrz", "ppyypllr")) # false
+print(solution.isLongPressedName("alex", "alexxr")) # false
+print(solution.isLongPressedName("alex", "aaleex")) # true
+print(solution.isLongPressedName("saeed", "ssaaedd")) # false
+print(solution.isLongPressedName("leelee", "lleeelee")) # true
+print(solution.isLongPressedName("laiden", "laiden")) # true
