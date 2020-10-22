400bed5888ee9d1be500eb8117bb2eea70af8b61
Patrick Murphy <43220681+DiningRoomChair@users.noreply.github.com>
2020-10-22 14:00:12 -0400

    434 - Number of Segments in a String

---
+++ Leetcode/Easy/C#/434 - Number of Segments in a String/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _434___Number_of_Segments_in_a_String
+{
+    class Program
+    {
+        public class Solution
+        {
+            public int CountSegments(string s) {
+                if(s == ""){
+                    return 0;
+                }
+                int segmentCount = 0;
+                bool wasLastCharASpace;
+                if(s[0] == ' '){
+                    wasLastCharASpace = true;
+                }
+                else{
+                    segmentCount++;
+                    wasLastCharASpace = false;
+                }
+                foreach(char c in s){
+                    if(c != ' '){
+                        if(wasLastCharASpace){
+                            segmentCount++;
+                            wasLastCharASpace = false;
+                        }
+                    }
+                    else{
+                        wasLastCharASpace = true;
+                    }
+                }
+                return segmentCount;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            Console.WriteLine(solution.CountSegments("Hello, my name is John")); // 5
+            Console.WriteLine(solution.CountSegments("Hello")); // 1
+            Console.WriteLine(solution.CountSegments("love live! mu'sic forever")); // 4
+            Console.WriteLine(solution.CountSegments("")); // 0
+        }
+    }
+}
